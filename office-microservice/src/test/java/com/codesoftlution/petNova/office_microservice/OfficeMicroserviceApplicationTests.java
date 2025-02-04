53c28d1806211f63be2b8c31ef8fbb4c608951ef
Borys <borysjair96@gmail.com>
2025-02-04 18:43:54 -0500

    Ready office-microservice and calling with feignClient to officeById when created user Veterinary.

---
+++ office-microservice/src/test/java/com/codesoftlution/petNova/office_microservice/OfficeMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesoftlution.petNova.office_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
