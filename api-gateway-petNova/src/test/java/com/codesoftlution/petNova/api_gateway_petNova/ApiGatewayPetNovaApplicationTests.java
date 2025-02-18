c9eca4a933225394b2e9951238fe5c5fc6649711
Borys <borysjair96@gmail.com>
2025-02-17 21:20:53 -0500

    Instalada el Api Gateway pero aun no funciona porque la seguiridad esta en el user service

---
+++ api-gateway-petNova/src/test/java/com/codesoftlution/petNova/api_gateway_petNova/ApiGatewayPetNovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesoftlution.petNova.api_gateway_petNova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayPetNovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
