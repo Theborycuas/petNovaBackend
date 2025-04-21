b42dd915f1436df4164aeee103d0b0536746d96a
Borys <borysjair96@gmail.com>
2025-04-21 07:29:20 -0500

    Manejo de error con credenciales incorrectas.

--- user-microservice/src/main/java/com/codesoftlution/petNova/user_microservice/services/AuthService.java
+++ user-microservice/src/main/java/com/codesoftlution/petNova/user_microservice/services/AuthService.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -79,12 +80,18 @@ public class AuthService {
     }
 
     public AuthResponse authenticate(AuthRequest request) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.getUsername(),
-                        request.getPassword()
-                )
-        );
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getUsername(),
+                            request.getPassword()
+                    )
+            );
+        } catch (BadCredentialsException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "USUARIO O CONTRASEÑA INCORRECTOS");
+        }
+
+
         var user = userRepository.findByUsername(request.getUsername())
                 .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "USUARIO NO ENCONTRADO"));
         var jwtToken = jwtService.generateToken(user);
