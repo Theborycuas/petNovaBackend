40d74459824751aebcce8a333724ac662eef1246
Borys <borysjair96@gmail.com>
2025-04-28 11:27:12 -0500

    add new tenant microservice

---
+++ tenat-microservice/src/main/java/com/codesoftlution/petNova/tenat_microservice/TenatMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.codesoftlution.petNova.tenat_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenatMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenatMicroserviceApplication.class, args);
+	}
+
+}
---
+++ tenat-microservice/src/test/java/com/codesoftlution/petNova/tenat_microservice/TenatMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesoftlution.petNova.tenat_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenatMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
