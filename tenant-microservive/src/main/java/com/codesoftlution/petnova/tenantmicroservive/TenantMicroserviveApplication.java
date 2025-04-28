06efb2deaaff882c43977ef53ab462008614a9ba
Borys <borysjair96@gmail.com>
2025-04-28 13:27:04 -0500

    add new tenant microservice

---
+++ tenant-microservive/src/main/java/com/codesoftlution/petnova/tenantmicroservive/TenantMicroserviveApplication.java
@@ -0,0 +1,13 @@
+package com.codesoftlution.petnova.tenantmicroservive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantMicroserviveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenantMicroserviveApplication.class, args);
+	}
+
+}
---
+++ tenant-microservive/src/test/java/com/codesoftlution/petnova/tenantmicroservive/TenantMicroserviveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesoftlution.petnova.tenantmicroservive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantMicroserviveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
